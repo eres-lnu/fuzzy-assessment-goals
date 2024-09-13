e6027668781e15575bd414ba5c490236e58b56a5
Diego Perez <diego.perez@lnu.se>
2024-09-13 22:10:11 +0200

    Fix: Bug when the result assessment had discontinuities

--- src/main/java/se/lnu/eres/fuzzy_assessment_goals/functions/impl/LinearPieceWiseFunctionDataPoints.java
+++ src/main/java/se/lnu/eres/fuzzy_assessment_goals/functions/impl/LinearPieceWiseFunctionDataPoints.java
@@ -40,7 +40,7 @@ import se.lnu.eres.fuzzy_assessment_goals.functions.exceptions.FunctionOperation
 public class LinearPieceWiseFunctionDataPoints implements Iterable<ImmutablePair<Double, Double>> {
 
 	private static final Logger Logger = LogManager.getLogger(LinearPieceWiseFunctionDataPoints.class.getSimpleName());
-	
+
 	private List<ImmutablePair<Double, Double>> datapoints;
 
 	public List<ImmutablePair<Double, Double>> getDatapoints() {
@@ -59,14 +59,14 @@ public class LinearPieceWiseFunctionDataPoints implements Iterable<ImmutablePair
 
 	public LinearPieceWiseFunctionDataPoints(ImmutablePair<Double, Double>... points) {
 		this();
-		for(ImmutablePair<Double, Double> point : points) {
+		for (ImmutablePair<Double, Double> point : points) {
 			datapoints.add(point);
 		}
 	}
 
 	public void add(ImmutablePair<Double, Double> point) {
 		datapoints.add(point);
-		
+
 	}
 
 	public int size() {
@@ -74,7 +74,7 @@ public class LinearPieceWiseFunctionDataPoints implements Iterable<ImmutablePair
 	}
 
 	public ImmutablePair<Double, Double> get(int i) {
-	
+
 		return datapoints.get(i);
 	}
 
@@ -84,7 +84,7 @@ public class LinearPieceWiseFunctionDataPoints implements Iterable<ImmutablePair
 
 	public void add(int i, ImmutablePair<Double, Double> point) {
 		datapoints.add(i, point);
-		
+
 	}
 
 	@Override
@@ -102,44 +102,52 @@ public class LinearPieceWiseFunctionDataPoints implements Iterable<ImmutablePair
 
 	public List<Double> getXpoints() {
 		List<Double> xPoints = new ArrayList<Double>();
-		for(ImmutablePair<Double, Double> p : datapoints) {
+		for (ImmutablePair<Double, Double> p : datapoints) {
 			xPoints.add(p.getLeft());
 		}
 		return xPoints;
 	}
 
+	/**
+	 * @param point
+	 * @return The first interval containing the x value of the point. Therefore, it
+	 *         does not return the correct value if it is a point where the function
+	 *         is not continuous
+	 * @throws FunctionOperationException
+	 */
 	public LinearPieceWiseFunctionDataPoints getIntervalContaining(double point) throws FunctionOperationException {
-		
-		ImmutablePair<Double, Double> left=null;
-		boolean isLeftSet=false;
-		
-		for(ImmutablePair<Double, Double> right : datapoints) {
-			if(!isLeftSet) {
-				left=right;
-				isLeftSet=true;
-			}
-			else {
-				if(left.getLeft()<=point && right.getLeft()>=point) {
-					Logger.debug("Possible problem with the Varargs in the constructor. Calling with <point,left,right>=<{},{},{}>", point,left,right);
-					return new LinearPieceWiseFunctionDataPoints(left,right);
+
+		ImmutablePair<Double, Double> left = null;
+		boolean isLeftSet = false;
+
+		for (ImmutablePair<Double, Double> right : datapoints) {
+			if (!isLeftSet) {
+				left = right;
+				isLeftSet = true;
+			} else {
+				if (left.getLeft() <= point && right.getLeft() >= point) {
+					Logger.debug(
+							"Possible problem with the Varargs in the constructor. Calling with <point,left,right>=<{},{},{}>",
+							point, left, right);
+					return new LinearPieceWiseFunctionDataPoints(left, right);
 				}
-				left=right;
+				left = right;
 			}
 		}
-		
-		throw new FunctionOperationException("Interval for point " + point + " was not found in dataset=" + datapoints.toString());
+
+		throw new FunctionOperationException(
+				"Interval for point " + point + " was not found in dataset=" + datapoints.toString());
 	}
 
 	public void addAll(LinearPieceWiseFunctionDataPoints additionalData) {
 		datapoints.addAll(additionalData.datapoints);
-		
+
 	}
 
 	@Override
 	public String toString() {
 		return "LinearPieceWiseFunctionDataPoints [datapoints=" + datapoints.toString() + "]";
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -155,98 +163,253 @@ public class LinearPieceWiseFunctionDataPoints implements Iterable<ImmutablePair
 
 	public void sortByX() {
 		Collections.sort(datapoints, new XPointsComparator());
+
+	}
+
+	/**
+	 * This function assumes that the datapoints represent intervals in couples such
+	 * as: interval1= [datapoint0, datapoint1], interval2= [datapoint2,datapoint3]
+	 * ...
+	 */
+	public void sortByXRespectingIntervalCouples() {
+		// TODO Auto-generated method stub
+
+		List<ImmutablePair<Double, Double>> newdatapoints = new ArrayList<ImmutablePair<Double, Double>>();
+		
+		while(datapoints.size()>0) {
+			newdatapoints.addAll(removeLowestXStartingIntervalFromDatapoints());
+		}
+		datapoints=newdatapoints;
+	}
+
+	private List<ImmutablePair<Double, Double>> removeLowestXStartingIntervalFromDatapoints() {
+		int indexSmallestBeginningOfInterval=0;
+		double smallestX=Double.MAX_VALUE;
+		for(int i=0; i<datapoints.size(); i++) {
+			if((i%2)==0) {//Beggining of interval
+				if(datapoints.get(i).getLeft()<smallestX) {
+					smallestX=datapoints.get(i).getLeft();
+					indexSmallestBeginningOfInterval=i;
+				}
+			}
+		}
+		List<ImmutablePair<Double, Double>> result = new ArrayList<ImmutablePair<Double, Double>>();
+		result.addLast(remove(indexSmallestBeginningOfInterval+1));
+		result.addFirst(remove(indexSmallestBeginningOfInterval));
+		return result;
+		
 		
 	}
-	
-	class XPointsComparator implements Comparator<ImmutablePair<Double,Double>>{
+
+	class XPointsComparator implements Comparator<ImmutablePair<Double, Double>> {
 
 		@Override
 		public int compare(ImmutablePair<Double, Double> o1, ImmutablePair<Double, Double> o2) {
-			//Compares its two arguments for order. Returns a negative integer, zero, or a positive integer as the first argument is less than, equal to, or greater than the second.
-			if(o1.getLeft()<o2.getLeft()) {return -1;}
-			if(o1.getLeft().equals(o2.getLeft())) {return 0;}
-			else return 1;
+			// Compares its two arguments for order. Returns a negative integer, zero, or a
+			// positive integer as the first argument is less than, equal to, or greater
+			// than the second.
+			if (o1.getLeft() < o2.getLeft()) {
+				return -1;
+			}
+			if (o1.getLeft().equals(o2.getLeft())) {
+				return 0;
+			} else
+				return 1;
 		}
-		
+
 	}
 
 	public void retainLargestYforReplicatedX() {
-		//It asumes that the elements are sorted by X
+		// It asumes that the elements are sorted by X
 		List<ImmutablePair<Double, Double>> newDatapoints = new ArrayList<ImmutablePair<Double, Double>>();
-		
-		int datapointsLength=datapoints.size();
-		
-		while(datapointsLength>0) {
+
+		int datapointsLength = datapoints.size();
+
+		while (datapointsLength > 0) {
 			double currentProcessingX = datapoints.getLast().getLeft();
 			double maxY = getMaximumYforCurrentX(currentProcessingX);
-			
-			//Not good in terms of time complexity, to use addFist when the type is an ArrayList.
-			newDatapoints.addFirst(new ImmutablePair<Double,Double>(currentProcessingX,maxY));
+
+			// Not good in terms of time complexity, to use addFist when the type is an
+			// ArrayList.
+			newDatapoints.addFirst(new ImmutablePair<Double, Double>(currentProcessingX, maxY));
 			removeXvaluesFromLast(currentProcessingX);
-			
-			datapointsLength=datapoints.size();
+
+			datapointsLength = datapoints.size();
 		}
-		
-		datapoints=newDatapoints;
-		
+
+		datapoints = newDatapoints;
+
 	}
 
 	private void removeXvaluesFromLast(double currentProcessingX) {
-		//Removing from the last because it gives less room to make mess with the indices when iterating.
-		while(datapoints.size()>0 && DoubleMath.fuzzyEquals(datapoints.getLast().getLeft(), currentProcessingX, LinearPieceWiseFunction.TOLERANCE)) {
+		// Removing from the last because it gives less room to make mess with the
+		// indices when iterating.
+		while (datapoints.size() > 0 && DoubleMath.fuzzyEquals(datapoints.getLast().getLeft(), currentProcessingX,
+				LinearPieceWiseFunction.TOLERANCE)) {
 			datapoints.removeLast();
 		}
-		
+
 	}
 
 	private double getMaximumYforCurrentX(double currentProcessingX) {
 		double maxY = -Double.MAX_VALUE;
-		
-		for(int i = datapoints.size()-1; i>=0; i--) {
-			if(datapoints.get(i).getLeft().equals(currentProcessingX)) {
-				if(datapoints.get(i).getRight()>maxY) {
-					maxY=datapoints.get(i).getRight();
+
+		for (int i = datapoints.size() - 1; i >= 0; i--) {
+			if (datapoints.get(i).getLeft().equals(currentProcessingX)) {
+				if (datapoints.get(i).getRight() > maxY) {
+					maxY = datapoints.get(i).getRight();
 				}
+			} else {
+				return maxY;
 			}
-			else {return maxY;}
-			
-		} 
+
+		}
 		return maxY;
 	}
 
 	public void removeIntermediatePoitnsForLinearFunctions() throws FunctionOperationException {
-		//This function assumes that the datapoints are sorted
-		if(datapoints.size()<3) {return;}
-		ImmutablePair<Double,Double> left, middle, right;
-		
-		
-		left=datapoints.get(0);
-		middle=datapoints.get(1);
-		
-		int i=2;	
-		while(i<datapoints.size()) {
-			right = datapoints.get(i);
-			double yFromFunction = (new FunctionPiecewiseImpl(new LinearPieceWiseFunctionDataPoints(left,right))).getValueAt(middle.getLeft());
-			Logger.info("Trying to remove intermediate point {} between values {} and {}. ", middle, left, right);
-			
-			if(DoubleMath.fuzzyEquals(yFromFunction, middle.getRight(), LinearPieceWiseFunction.TOLERANCE)) {
-			Logger.info("Removing point because the Y at point {} is {} , which is equal to {}", middle.getLeft(), yFromFunction, middle.getRight());
-				datapoints.remove(i-1);
-				middle=right;
-			}
-			else {
-				Logger.info("Leaving point because the Y at point {} is {} , which is NOT equal to {}", middle.getLeft(), yFromFunction, middle.getRight());
-				left=middle;
-				middle=right;
-				i++;
-				}
+		// This function assumes that the datapoints are sorted
+		if (datapoints.size() < 3) {
+			return;
 		}
-		
-	}
-		
-		
+		ImmutablePair<Double, Double> left, middle, right;
 
-	
-	
+		left = datapoints.get(0);
+		middle = datapoints.get(1);
+
+		int i = 2;
+		while (i < datapoints.size()) {
+			right = datapoints.get(i);
+			double yFromFunction = (new FunctionPiecewiseImpl(new LinearPieceWiseFunctionDataPoints(left, right)))
+					.getValueAt(middle.getLeft());
+			Logger.info("Trying to remove intermediate point {} between values {} and {}. ", middle, left, right);
+
+			if (DoubleMath.fuzzyEquals(yFromFunction, middle.getRight(), LinearPieceWiseFunction.TOLERANCE)) {
+				Logger.info("Removing point because the Y at point {} is {} , which is equal to {}", middle.getLeft(),
+						yFromFunction, middle.getRight());
+				datapoints.remove(i - 1);
+				middle = right;
+			} else {
+				Logger.info("Leaving point because the Y at point {} is {} , which is NOT equal to {}",
+						middle.getLeft(), yFromFunction, middle.getRight());
+				left = middle;
+				middle = right;
+				i++;
+			}
+		}
+
+	}
+
+	public void removeDuplicatedPoints() {
+		int size = datapoints.size();
+		for (int i = 0; i < size; i++) {
+			for (int j = size - 1; j > i; j--) {
+				// Use the relaxed comparator with some tolerance
+				// if (datapoints.get(i).equals(datapoints.get(j))) {
+				if (myEqualDatapoints(datapoints.get(i), datapoints.get(j))) {
+					remove(j);
+					size--;
+				}
+			}
+
+		}
+
+	}
+
+	/**
+	 * Removes point in position i only if the i-1 has the same <x,y> value
+	 */
+	public void removeDuplicatedNeighborPoints() {
+		int size = datapoints.size();
+		for (int i = size - 1; i > 0; i--) {
+			// Use the relaxed comparator with some tolerance
+			if (myEqualDatapoints(datapoints.get(i), datapoints.get(i - 1))) {
+				// if(datapoints.get(i).equals(datapoints.get(i-1))) {
+				remove(i);
+			}
+		}
+
+	}
+
+	private boolean myEqualDatapoints(ImmutablePair<Double, Double> p1, ImmutablePair<Double, Double> p2) {
+
+		if (!DoubleMath.fuzzyEquals(p1.getLeft(), p2.getLeft(), LinearPieceWiseFunction.TOLERANCE)) {
+			return false;
+		}
+		return DoubleMath.fuzzyEquals(p1.getRight(), p2.getRight(), LinearPieceWiseFunction.TOLERANCE);
+	}
+
+	/**
+	 * Add in even positions to avoid putting the single point in between the two
+	 * points of an interval. This method should be called only when the Datapoints
+	 * in the function are duplicated to indicate intervals, such as: interval1=
+	 * [datapoint0, datapoint1], interval2= [datapoint2,datapoint3] ...
+	 * 
+	 * A single datapoint in points2 should not break any interval. That means that
+	 * its left value (the x value) should be between a point in position 0 or
+	 * (2n-1) and 2n for n in 1,2,3, and NOT between a point in position 2n and 2n+1
+	 * for n in 0,1,2...
+	 * 
+	 * @param the DatPoints to merge
+	 */
+	public void addAllSortedInEvenPositions(LinearPieceWiseFunctionDataPoints points2) {
+		for (ImmutablePair<Double, Double> p : points2.getDatapoints().reversed()) { // From largest to smalles
+			// from the end of the datapoints to keep parity in the next positions to treat
+			boolean added = false;
+			for (int i = datapoints.size() - 1; (i >= 0) && (!added); i--) {
+				if (datapoints.get(i).getLeft() <= p.getLeft()) {
+					// Position found: add on the righ if n is odd and on the left (same position i
+					// because the rest are moved to the right) if n is even.
+					if ((i % 2) == 1) {
+						datapoints.add(i + 1, p);
+					} else {
+						datapoints.add(i, p);
+					}
+
+					added = true;
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method removes the point <x,y> if it finds in the dataset a points
+	 * <x,y_y> and all y_y are larger than y
+	 * 
+	 * @param d2 The second datapoints
+	 */
+	public void removePointsWhoseYValueisTheSmallest(LinearPieceWiseFunctionDataPoints d2) {
+		int size = datapoints.size();
+		for (int i = size - 1; i >= 0; i--) {
+			if (d2.valueXisAnIntervalExtreme(datapoints.get(i).getLeft())) {
+				ImmutablePair<Double, Double> smallest = d2.findLowestIntervalExtremeForX(datapoints.get(i).getLeft());
+				if (smallest.getRight() > datapoints.get(i).getRight()) {// the point in this function was the smallest
+					remove(i);
+				}
+
+			}
+		}
+
+	}
+
+	private boolean valueXisAnIntervalExtreme(double x) {
+		for (ImmutablePair<Double, Double> point : getDatapoints()) {
+			if (DoubleMath.fuzzyEquals(point.getLeft(), x, LinearPieceWiseFunction.TOLERANCE)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private ImmutablePair<Double, Double> findLowestIntervalExtremeForX(Double left) {
+		ImmutablePair<Double, Double> smallestY = new ImmutablePair<Double, Double>(left, Double.MAX_VALUE);
+		for (ImmutablePair<Double, Double> point : getDatapoints()) {
+			if (DoubleMath.fuzzyEquals(left, point.getLeft(), LinearPieceWiseFunction.TOLERANCE)
+					&& point.getRight() < smallestY.getRight()) {
+				smallestY = point;
+			}
+		}
+		return smallestY;
+	}
 
 }
--- src/test/java/se/lnu/eres/fuzzy_assessment_goals/goals/impl/LeafGoalImplTest.java
+++ src/test/java/se/lnu/eres/fuzzy_assessment_goals/goals/impl/LeafGoalImplTest.java
@@ -21,12 +21,13 @@
  */
 package se.lnu.eres.fuzzy_assessment_goals.goals.impl;
 
-
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.google.common.math.DoubleMath;
 
 import se.lnu.eres.fuzzy_assessment_goals.functions.impl.FuzzyNumberImpl;
 import se.lnu.eres.fuzzy_assessment_goals.functions.FuzzyBoolean;
@@ -36,7 +37,6 @@ import se.lnu.eres.fuzzy_assessment_goals.goals.LeafGoalType;
 import se.lnu.eres.fuzzy_assessment_goals.functions.impl.FunctionPiecewiseImpl;
 import se.lnu.eres.fuzzy_assessment_goals.functions.LinearPieceWiseFunction;
 import se.lnu.eres.fuzzy_assessment_goals.functions.exceptions.FunctionOperationException;
-
 
 class LeafGoalImplTest {
 
@@ -51,7 +51,7 @@ class LeafGoalImplTest {
 	@Test
 	void testAssessSatisfaction() throws FunctionOperationException {
 		LinearPieceWiseFunction function = new FunctionPiecewiseImpl();
-		//The points in the longitudinal acceleration satisfaction
+		// The points in the longitudinal acceleration satisfaction
 		function.addPoint(0.0, 1.0);
 		function.addPoint(0.56, 1.0);
 		function.addPoint(1.89, 0.0);
@@ -59,27 +59,232 @@ class LeafGoalImplTest {
 		FuzzyNumber goalTruthValue = new FuzzyNumberImpl(function);
 
 		LeafGoal goal = new LeafGoalImpl(LeafGoalType.UB, goalTruthValue);
-		
+
 		LinearPieceWiseFunction observationFunction = new FunctionPiecewiseImpl();
-		//The points in the Observation O2. Triangle with Max in f(0.7)=1 and mins in f(0.5)=f(0.9)=0
+		// The points in the Observation O2. Triangle with Max in f(0.7)=1 and mins in
+		// f(0.5)=f(0.9)=0
 		observationFunction.addPoint(0.0, 0.0);
 		observationFunction.addPoint(0.5, 0.0);
 		observationFunction.addPoint(0.7, 1.0);
 		observationFunction.addPoint(0.9, 0.0);
 		observationFunction.addPoint(Double.MAX_VALUE, 0.0);
 		FuzzyNumber observation = new FuzzyNumberImpl(observationFunction);
-		
+
 		goal.setObservation(observation);
 		FuzzyBoolean result = goal.assessSatisfaction();
 		Assertions.assertEquals(4, result.getFunction().getDatapoints().size());
-		for(ImmutablePair<Double,Double> point : result.getFunction().getDatapoints()) {
-		Assertions.assertTrue(Double.isFinite(point.getLeft()), "There is an X point in the result whose value is not finite, its value is: " + point.getLeft());
-		Assertions.assertTrue(Double.isFinite(point.getRight()), "There is an Y point in the result whose value is not finite, its value is: " + point.getRight());
+		for (ImmutablePair<Double, Double> point : result.getFunction().getDatapoints()) {
+			Assertions.assertTrue(Double.isFinite(point.getLeft()),
+					"There is an X point in the result whose value is not finite, its value is: " + point.getLeft());
+			Assertions.assertTrue(Double.isFinite(point.getRight()),
+					"There is an Y point in the result whose value is not finite, its value is: " + point.getRight());
 		}
-		
+
 		System.out.println("Result of goal satisfaction is:" + result.toString());
-		
+
+		double[] x = new double[]{0, 0.74436, 0.894736, 1.0};
+		double[] y = new double[]{0, 0, 1.0, 0.3};
+		for(int i=0; i<x.length;i++) {
+			Assertions.assertTrue(DoubleMath.fuzzyEquals(x[i], result.getFunction().getDatapoints().get(i).getLeft(), LinearPieceWiseFunction.TOLERANCE));
+			Assertions.assertTrue(DoubleMath.fuzzyEquals(y[i], result.getFunction().getDatapoints().get(i).getRight(), LinearPieceWiseFunction.TOLERANCE));
+		}
 		
 	}
 
+	@Test
+	void testAssessFullSatisfaction() throws FunctionOperationException {
+		LinearPieceWiseFunction function = new FunctionPiecewiseImpl();
+		// The points in the longitudinal acceleration satisfaction
+		function.addPoint(0.0, 1.0);
+		function.addPoint(0.56, 1.0);
+		function.addPoint(1.89, 0.0);
+		function.addPoint(Double.MAX_VALUE, 0.0);
+		FuzzyNumber goalTruthValue = new FuzzyNumberImpl(function);
+
+		LeafGoal goal = new LeafGoalImpl(LeafGoalType.UB, goalTruthValue);
+
+		LinearPieceWiseFunction observationFunction = new FunctionPiecewiseImpl();
+		// The points in the Observation O2. Triangle with Max in f(0.7)=1 and mins in
+		// f(0.5)=f(0.9)=0
+		observationFunction.addPoint(0.0, 0.0);
+		observationFunction.addPoint(0.1, 0.0);
+		observationFunction.addPoint(0.2, 1.0);
+		observationFunction.addPoint(0.3, 0.0);
+		observationFunction.addPoint(Double.MAX_VALUE, 0.0);
+		FuzzyNumber observation = new FuzzyNumberImpl(observationFunction);
+
+		goal.setObservation(observation);
+		FuzzyBoolean result = goal.assessSatisfaction();
+		Assertions.assertEquals(3, result.getFunction().getDatapoints().size());
+		for (ImmutablePair<Double, Double> point : result.getFunction().getDatapoints()) {
+			Assertions.assertTrue(Double.isFinite(point.getLeft()),
+					"There is an X point in the result whose value is not finite, its value is: " + point.getLeft());
+			Assertions.assertTrue(Double.isFinite(point.getRight()),
+					"There is an Y point in the result whose value is not finite, its value is: " + point.getRight());
+		}
+		Assertions.assertEquals(result.getFunction().getDatapoints().get(0), new ImmutablePair<Double,Double>(0.0,0.0));
+		Assertions.assertEquals(result.getFunction().getDatapoints().get(1), new ImmutablePair<Double,Double>(1.0,0.0));
+		Assertions.assertEquals(result.getFunction().getDatapoints().get(2), new ImmutablePair<Double,Double>(1.0,1.0));
+
+		System.out.println("Result of goal satisfaction is:" + result.toString());
+
+	}
+
+	@Test
+	void testAssessFullUnSatisfaction() throws FunctionOperationException {
+		LinearPieceWiseFunction function = new FunctionPiecewiseImpl();
+		// The points in the longitudinal acceleration satisfaction
+		function.addPoint(0.0, 1.0);
+		function.addPoint(0.56, 1.0);
+		function.addPoint(1.89, 0.0);
+		function.addPoint(Double.MAX_VALUE, 0.0);
+		FuzzyNumber goalTruthValue = new FuzzyNumberImpl(function);
+
+		LeafGoal goal = new LeafGoalImpl(LeafGoalType.UB, goalTruthValue);
+
+		LinearPieceWiseFunction observationFunction = new FunctionPiecewiseImpl();
+		// The points in the Observation O2. Triangle with Max in f(0.7)=1 and mins in
+		// f(0.5)=f(0.9)=0
+		observationFunction.addPoint(0.0, 0.0);
+		observationFunction.addPoint(2, 0.0);
+		observationFunction.addPoint(3, 1.0);
+		observationFunction.addPoint(4, 0.0);
+		observationFunction.addPoint(Double.MAX_VALUE, 0.0);
+		FuzzyNumber observation = new FuzzyNumberImpl(observationFunction);
+
+		goal.setObservation(observation);
+		FuzzyBoolean result = goal.assessSatisfaction();
+		Assertions.assertEquals(3, result.getFunction().getDatapoints().size(), " the result contains: " + result.getFunction().toString());
+		for (ImmutablePair<Double, Double> point : result.getFunction().getDatapoints()) {
+			Assertions.assertTrue(Double.isFinite(point.getLeft()),
+					"There is an X point in the result whose value is not finite, its value is: " + point.getLeft());
+			Assertions.assertTrue(Double.isFinite(point.getRight()),
+					"There is an Y point in the result whose value is not finite, its value is: " + point.getRight());
+		}
+
+		System.out.println("Result of goal satisfaction is:" + result.toString());
+		Assertions.assertEquals(result.getFunction().getDatapoints().get(0), new ImmutablePair<Double,Double>(0.0,1.0));
+		Assertions.assertEquals(result.getFunction().getDatapoints().get(1), new ImmutablePair<Double,Double>(0.0,0.0));
+		Assertions.assertEquals(result.getFunction().getDatapoints().get(2), new ImmutablePair<Double,Double>(1.0,0.0));
+
+	}
+
+	@Test
+	void testAssessFullUncertainSatisfaction() throws FunctionOperationException {
+		LinearPieceWiseFunction function = new FunctionPiecewiseImpl();
+		// The points in the longitudinal acceleration satisfaction
+		function.addPoint(0.0, 1.0);
+		function.addPoint(0.56, 1.0);
+		function.addPoint(1.89, 0.0);
+		function.addPoint(Double.MAX_VALUE, 0.0);
+		FuzzyNumber goalTruthValue = new FuzzyNumberImpl(function);
+
+		LeafGoal goal = new LeafGoalImpl(LeafGoalType.UB, goalTruthValue);
+
+		LinearPieceWiseFunction observationFunction = new FunctionPiecewiseImpl();
+		// The points in the Observation O2. Triangle with Max in f(0.7)=1 and mins in
+		// f(0.5)=f(0.9)=0
+		observationFunction.addPoint(0.0, 0.0);
+		observationFunction.addPoint(0.1, 0.0);
+		observationFunction.addPoint(0.2, 1.0);
+		observationFunction.addPoint(2.0, 1.0);
+		observationFunction.addPoint(3.0, 0.0);
+		observationFunction.addPoint(Double.MAX_VALUE, 0.0);
+		FuzzyNumber observation = new FuzzyNumberImpl(observationFunction);
+
+		goal.setObservation(observation);
+		FuzzyBoolean result = goal.assessSatisfaction();
+		Assertions.assertEquals(2, result.getFunction().getDatapoints().size());
+		for (ImmutablePair<Double, Double> point : result.getFunction().getDatapoints()) {
+			Assertions.assertTrue(Double.isFinite(point.getLeft()),
+					"There is an X point in the result whose value is not finite, its value is: " + point.getLeft());
+			Assertions.assertTrue(Double.isFinite(point.getRight()),
+					"There is an Y point in the result whose value is not finite, its value is: " + point.getRight());
+		}
+
+		System.out.println("Result of goal satisfaction is:" + result.toString());
+
+
+	}
+	
+	
+	@Test
+	void testAssessPointDiscontinuityInMiddleSmallIncrementObservation() throws FunctionOperationException {
+		LinearPieceWiseFunction function = new FunctionPiecewiseImpl();
+		// The points in the longitudinal acceleration satisfaction
+		function.addPoint(0.0, 1.0);
+		function.addPoint(0.5, 1.0);
+		function.addPoint(1.0, 0.5);
+		function.addPoint(1.5, 0.5);
+		function.addPoint(2.0, 0.0);
+		function.addPoint(Double.MAX_VALUE, 0.0);
+		FuzzyNumber goalTruthValue = new FuzzyNumberImpl(function);
+
+		LeafGoal goal = new LeafGoalImpl(LeafGoalType.UB, goalTruthValue);
+
+		LinearPieceWiseFunction observationFunction = new FunctionPiecewiseImpl();
+		// The points in the Observation O2. Triangle with Max in f(0.7)=1 and mins in
+		// f(0.5)=f(0.9)=0
+		observationFunction.addPoint(0.0, 0.0);
+		observationFunction.addPoint(0.6, 0.0);
+		observationFunction.addPoint(1.2, 1.0);
+		observationFunction.addPoint(5.0, 0.0);
+		observationFunction.addPoint(Double.MAX_VALUE, 0.0);
+		FuzzyNumber observation = new FuzzyNumberImpl(observationFunction);
+
+		goal.setObservation(observation);
+		FuzzyBoolean result = goal.assessSatisfaction();
+		Assertions.assertEquals(6, result.getFunction().getDatapoints().size(), " the result contains: " + result.getFunction().toString());
+		for (ImmutablePair<Double, Double> point : result.getFunction().getDatapoints()) {
+			Assertions.assertTrue(Double.isFinite(point.getLeft()),
+					"There is an X point in the result whose value is not finite, its value is: " + point.getLeft());
+			Assertions.assertTrue(Double.isFinite(point.getRight()),
+					"There is an Y point in the result whose value is not finite, its value is: " + point.getRight());
+		}
+
+		System.out.println("Result of goal satisfaction is:" + result.toString());
+
+
+	}
+
+	
+	@Test
+	void testAssessPointDiscontinuityInMiddleFastIncrementObservation() throws FunctionOperationException {
+		LinearPieceWiseFunction function = new FunctionPiecewiseImpl();
+		// The points in the longitudinal acceleration satisfaction
+		function.addPoint(0.0, 1.0);
+		function.addPoint(0.5, 1.0);
+		function.addPoint(1.0, 0.5);
+		function.addPoint(1.5, 0.5);
+		function.addPoint(2.0, 0.0);
+		function.addPoint(Double.MAX_VALUE, 0.0);
+		FuzzyNumber goalTruthValue = new FuzzyNumberImpl(function);
+
+		LeafGoal goal = new LeafGoalImpl(LeafGoalType.UB, goalTruthValue);
+
+		LinearPieceWiseFunction observationFunction = new FunctionPiecewiseImpl();
+		// The points in the Observation O2. Triangle with Max in f(0.7)=1 and mins in
+		// f(0.5)=f(0.9)=0
+		observationFunction.addPoint(0.0, 0.0);
+		observationFunction.addPoint(0.9, 0.0);
+		observationFunction.addPoint(1.1, 1.0);
+		observationFunction.addPoint(20.0, 0.0);
+		observationFunction.addPoint(Double.MAX_VALUE, 0.0);
+		FuzzyNumber observation = new FuzzyNumberImpl(observationFunction);
+
+		goal.setObservation(observation);
+		FuzzyBoolean result = goal.assessSatisfaction();
+		Assertions.assertEquals(6, result.getFunction().getDatapoints().size(), " the result contains: " + result.getFunction().toString());
+		for (ImmutablePair<Double, Double> point : result.getFunction().getDatapoints()) {
+			Assertions.assertTrue(Double.isFinite(point.getLeft()),
+					"There is an X point in the result whose value is not finite, its value is: " + point.getLeft());
+			Assertions.assertTrue(Double.isFinite(point.getRight()),
+					"There is an Y point in the result whose value is not finite, its value is: " + point.getRight());
+		}
+
+		System.out.println("Result of goal satisfaction is:" + result.toString());
+
+
+	}
+	
 }
